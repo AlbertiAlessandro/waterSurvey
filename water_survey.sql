9aaae86b4df3e8508144f6cccc4a60d65dd01b10
nicolas mantelli <nicolasmantelli05@gmail.com>
2024-05-19 18:23:43 +0200

    merge dei due database

--- water_survey.sql
+++ water_survey.sql
@@ -40,19 +40,18 @@ create table survey
     constraint creatore
         foreign key (creatore) references user (id)
 );
-
-create table question
-(
-    id           int unsigned auto_increment
-        primary key,
-    domanda      text         not null,
-    tipo         int unsigned null,
-    questionario int unsigned null,
-    constraint questionario
-        foreign key (questionario) references survey (id),
-    constraint tipo
-        foreign key (tipo) references tipo_domanda (id)
-);
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE `question` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `domanda` text NOT NULL,
+  `tipo` int(10) unsigned DEFAULT NULL,
+  `id_survey` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `questionario` (`id_survey`),
+  KEY `tipo` (`tipo`),
+  CONSTRAINT `questionario` FOREIGN KEY (`id_survey`) REFERENCES `survey` (`id`),
+  CONSTRAINT `tipo` FOREIGN KEY (`tipo`) REFERENCES `tipo_domanda` (`id`)
+)
 
 create table response
 (
@@ -68,4 +67,19 @@ create table response
         foreign key (id_questionario) references survey (id),
     constraint response_user_id_fk
         foreign key (id_utente) references user (id)
+);
+CREATE TABLE `options` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `risposta` text DEFAULT NULL,
+  `id_utente` int(10) unsigned NOT NULL,
+  `id_domanda` int(10) unsigned NOT NULL,
+  `id_questionario` int(10) unsigned DEFAULT NULL,
+  `correct` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `options_question_id_fk` (`id_domanda`),
+  KEY `options_survey_id_fk` (`id_questionario`),
+  KEY `options_user_id_fk` (`id_utente`),
+  CONSTRAINT `options_question_id_fk` FOREIGN KEY (`id_domanda`) REFERENCES `question` (`id`),
+  CONSTRAINT `options_survey_id_fk` FOREIGN KEY (`id_questionario`) REFERENCES `survey` (`id`),
+  CONSTRAINT `options_user_id_fk` FOREIGN KEY (`id_utente`) REFERENCES `user` (`id`)
 );
